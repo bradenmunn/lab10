36c38fea6e4589a46719b0334e902c58a35a5507
bradenmunn <bradenmunn@yahoo.com>
2019-04-11 11:27:57 -0500

    Created Srawable interfact and added its methods to Shape

--- src/Shape.java
+++ src/Shape.java
@@ -1,4 +1,37 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
 
-public class Shape {
+public class Shape implements Drawable{
 
+	private Point[] location;
+	private Color color;
+	private boolean filled;
+	
+	public Shape(Color color, boolean filled)
+	{
+		this.color = color;
+		this.filled = filled;
+	}
+	
+	public Color getColor()
+	{
+		return color;
+	}
+	
+	public boolean isFilled()
+	{
+		return filled;
+	}
+	
+	public Point[] getLocation()
+	{
+		return location;
+	}
+
+	@Override
+	public void draw(Graphics graphics) {
+		// TODO Auto-generated method stub
+		
+	}
 }
